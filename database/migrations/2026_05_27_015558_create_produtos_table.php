206b85c07423334e1e8b8ac576ccf3b9eed11f9a
daniel-trd <danieltrindade036@gmail.com>
2026-05-26 23:30:24 -0300

    Migrations and Models Estoque

---
+++ database/migrations/2026_05_27_015558_create_produtos_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->id('id_produto');
+            $table->string('codigo_barras', 50)->unique();
+            $table->string('nome', 100);
+            $table->decimal('preco', 10, 2);
+            $table->marca('marca', 50)->nullable();
+            $table->text('descricao')->nullable();
+            $table->string('unidade', 20);
+            $table->boolean('controla_estoque')->default(true);
+            $table->integer('estoque_minimo')->default(0);
+            $table->integer('estoque_maximo')->default(0);
+            $table->boolean('ativo')->default(true);
+            $table->timestamp('created_at')->useCurrent();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('produtos');
+    }
+};
